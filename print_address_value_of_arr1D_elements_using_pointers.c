e8d64b28a33d608a5e064d860233be2339a4ba19
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-15 16:50:03 +0530

    print arr element using pointers

--- print_address_value_of_arr1D_elements_using_pointers.c
+++ print_address_value_of_arr1D_elements_using_pointers.c
@@ -2,11 +2,12 @@
 int main()
 {
   int arr[5]={2,5,7,8,9};
-  int i;
+  int i,*p;
+  p=arr;
   for(i=0;i<5;i++)
   {
-    printf("value of arr[%d]=%d\n",i,*(arr+i));
-     printf("address of arr[%d]=%d\n",i,(arr+i));
+    printf("value of arr[%d]=%d %d %d\n",i,*(arr+i),*(p+i),p[i]);
+     printf("address of arr[%d]=%d\n",i,(arr+i),p+i,&p[i]);
   }
-  return ;
+  return 0;
   }

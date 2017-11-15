df8818ccc4af3138b6a49a7d3d73c0c794264b99
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-15 17:33:01 +0530

    2D array pointer declaration

---
+++ 2D_arr_pointer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int arr[3][4]={{2,3,5,6},{3,6,0,7},{3,1,6,7}};
+  int (*p)[4];      //to point a 2D array[i][j] syntax of declaring a pointer p =(*p)[j]  
+  p=arr;
+  int i,j;
+  for(i=0;i<3;i++)
+  {
+    for(j=0;j<4;j++)
+   {
+	printf("address of arr[%d][%d]=%d\n",i,j,*(p+i)+j);
+    printf("value of arr[%d][%d]=%d\n\n",i,j,*(*(p+i)+j));
+    }
+	}
+    return 0;
+    }

5381ea886b37e4dbdd140b02e0b8cbad1bca9b2c
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-23 22:50:38 +0530

    dynamic memory allocation -realloc- pointers

---
+++ realloc.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *p,i;
+	
+	p=(int *)calloc(4,sizeof(int));
+	if(p==NULL)
+	{
+		printf("not enough space");
+		exit(1);
+	}
+	for(i=0;i<4;i++)
+	*(p+i)=i*2;
+	
+		p=(int *)realloc(p,8*sizeof(int)); //syntax of realloc() = (datatype *)realloc(pointer_name,new_size*sizeof(datatype))
+	if(p==NULL)
+	{
+		printf("not enough space");
+		exit(1);
+	}
+	for(i=4;i<8;i++)
+	*(p+i)=i*100;
+	
+		for(i=0;i<8;i++)
+		printf("%d\t",*(p+i));
+		
+		return 0;
+}

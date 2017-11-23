2fe111f1930572ce23460572dbf010b0168b45c7
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-23 22:38:52 +0530

    dynamic memory allocation-calloc-pointer

--- calloc.c
+++ calloc.c
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *p,n,i;
+	scanf("%d",&n);
+	p=(int *)calloc(n,sizeof(int));
+	if(p==NULL)
+	{
+		printf("not enough space");
+		exit(1);
+	}
+	for(i=0;i<n;i++)
+	scanf("%d",p+i);
+	
+		for(i=0;i<n;i++)
+		printf("%d",*(p+i));
+		
+		return 0;
+}

c10d64209ab52d2c7c8a9cc7d2768b8cfd430e99
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-23 12:58:12 +0530

    returning more than  one value from a function (pointer)

---
+++ ret_fn_pointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int ref(int a,int b,int *s,int *p,int *d);
+int main()
+
+{
+	int x=9,y=3,sum,prod,div;
+
+	ref(x,y,&sum,&prod,&div); //sending addresses to function
+	printf("%d %d %d\n",sum,prod,div); //returning values to the main fn
+	return 0;
+}
+int ref(int a,int b,int *s,int *p,int *d)
+{
+	*s=a+b;
+	*p=a*b;
+	*d=a/b;
+	
+}

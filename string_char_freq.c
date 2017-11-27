1cecf60124225b57607fd7618faa89fd3d91c4e1
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-27 10:59:36 +0530

    frequency of characters in string

--- string_char_freq.c
+++ string_char_freq.c
@@ -4,16 +4,35 @@ int main()
 {
  char a[100];
  gets(a);
- int b[26],i,j,l=97;
+ int b[26],c[26],i,j;
  for(i=0;i<26;i++)
- b[i]=0; // placing 0 at each block of array b to avoid garbage values.
- for(i=97;i<121;i++)
+ {
+ b[i]=0;// placing 0 at each block of array b&c to avoid garbage values.
+ c[i]=0;
+}
+ for(i=97;i<123;i++)
  for(j=0;j<strlen(a);j++)
  {
  	if(a[j]==i)
  	b[i-97]++;
  }
+  for(i=65;i<91;i++)
+ for(j=0;j<strlen(a);j++)
+ {
+ 	if(a[j]==i)
+ 	c[i-65]++;
+ }
  for(i=0;i<26;i++)
- printf("%c = %d\n",l++,b[i]);
+ {
+ if(b[i]==0)
+ continue;
+ printf("%c = %d\n",i+97,b[i]);
+}
+ for(i=0;i<26;i++)
+ {
+ if(c[i]==0)
+ continue;
+ printf("%c = %d\n",i+65,c[i]);
+}
  return 0;
 }

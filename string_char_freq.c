76ff500a38b86311cef8c6c8876cee3db92f2897
Harsh Sahu <33151203+dskaran95@users.noreply.github.com>
2017-11-27 10:43:01 +0530

    frequency of characters in an array

--- string_char_freq.c
+++ string_char_freq.c
@@ -2,20 +2,18 @@
 #include<string.h>
 int main()
 {
-	char a[100];
-	int i,j=0,k=97,l,b[27],m;
-	for(i=0;i<26;i++)
-	b[i]=0;
-	gets(a);
-	m=strlen(a);
-	for(i=97;i<121;i++)
-{
-		for(l=0;l<m;l++)
-	{
-		if(a[l]==i)
-		b[i-97]++;
-	}
-	}	for(i=0;i<26;i++)
-	printf("%c = %d\n",k++,b[i]);
-	return 0;
-	}
+ char a[100];
+ gets(a);
+ int b[26],i,j,l=97;
+ for(i=0;i<26;i++)
+ b[i]=0; // placing 0 at each block of array b to avoid garbage values.
+ for(i=97;i<121;i++)
+ for(j=0;j<strlen(a);j++)
+ {
+ 	if(a[j]==i)
+ 	b[i-97]++;
+ }
+ for(i=0;i<26;i++)
+ printf("%c = %d\n",l++,b[i]);
+ return 0;
+}
